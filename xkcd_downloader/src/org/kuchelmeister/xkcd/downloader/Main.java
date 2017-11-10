90f2e7787cc3b61d5501870f8c72af3bcb8b78df
Hannes <13hannes11@gmail.com>
2017-11-10 17:15:41 +0100

    added graphical folder chooser for image downloader

--- xkcd_downloader/src/org/kuchelmeister/xkcd/downloader/Main.java
+++ xkcd_downloader/src/org/kuchelmeister/xkcd/downloader/Main.java
@@ -1,13 +1,29 @@
 package org.kuchelmeister.xkcd.downloader;
 
+import javax.swing.JFileChooser;
+
 public class Main {
 
 	public static void main(final String[] args) {
 		final ImageFinder finder = new ImageFinder();
 		final ImageDownloader downloader = new ImageDownloader();
 
+		JFileChooser fileChooser;
+		fileChooser = new JFileChooser();
+
+		// FileOpenDialog
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		fileChooser.setDialogTitle("Select direcory to save in");
+		fileChooser.setMultiSelectionEnabled(false);
+		final int returnVal = fileChooser.showOpenDialog(null);
+
+		if (returnVal != JFileChooser.APPROVE_OPTION) {
+			System.out.println("Aborted!");
+			return;
+		}
+
 		finder.findImages();
-		downloader.downloadAllImages(finder.getImageURLs(), "C:/Users/Hannes/Pictures/xkcd/");
+		downloader.downloadAllImages(finder.getImageURLs(), fileChooser.getSelectedFile().getAbsolutePath());
 	}
 
 }
